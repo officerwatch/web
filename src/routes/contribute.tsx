e3a126f172d04124914337b827fe2cb20962a0b8
Sami <sfouad@gmail.com>
2022-05-27 10:18:58 -0600

    initial design

---
+++ src/routes/contribute.tsx
@@ -0,0 +1,9 @@
+function Contribute() {
+    return (
+      <main style={{ padding: "1rem 0" }}>
+        <h2>Contribute</h2>
+      </main>
+    );
+  }
+
+  export default Contribute;
---
+++ src/routes/report.tsx
@@ -0,0 +1,9 @@
+function Report() {
+    return (
+        <main style={{ padding: "1rem 0" }}>
+          <h2>Report</h2>
+        </main>
+      );
+}
+
+export default Report;
