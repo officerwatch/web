8b0228f44d08c18cdaa8cb85b6b921fc188b822a
Sami <sfouad@gmail.com>
2022-06-05 22:40:11 -0600

    removing some create-react-app bloat

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import Contribute from "./routes/contribute";
 import About from "./routes/about";
 import Contact from "./routes/contact";
@@ -25,8 +24,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
